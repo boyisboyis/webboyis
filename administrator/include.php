621fa5b6ec17fa0cbaaf438dd85cbd94f745a00d
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-07-20 23:45:53 +0700

    add main admin

--- administrator/include.php
+++ administrator/include.php
@@ -1,5 +1,8 @@
 <?php
-
+	
+	require_once ('../mainfunc/html.inc');
+	require_once ('../mainfunc/etc.inc');
+	require_once ('func/login_admin.inc');
 	require_once ('func/webadmin_style.inc');
 
 ?>
--- administrator/view.php
+++ administrator/view.php
@@ -1,7 +1,12 @@
 <?php
 	
 	require_once ('include.php');
+	$type_admin = 1;
 	$str = "";
-	echo admin_style($str);
+	if(!isset($_SESSION['admin_login']) || check_username_code($_SESSION['admin_login'])){
+		$type_admin = 0;
+		$str = admin_login_style();
+	}
+	echo admin_style($str, $type_admin);
 
 ?>
