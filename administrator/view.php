896ee8403f7025a102dc6dacec551f112db1e19c
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-08-10 23:22:48 +0700

    add form

--- administrator/view.php
+++ administrator/view.php
@@ -3,13 +3,24 @@
 	require_once ('include.php');
 	$type_admin = 1;
 	$str = "";
+	$etc_ar = array();
 	admin_only_computer();
 	if(!isset($_SESSION['admin_login']) || check_username_code($_SESSION['admin_login'])){
 		$type_admin = 0;
 		$str = admin_login_style();
 	}
 	else if(isset($_SESSION['admin_login'])){
-		$str = admin_controller_style();
+		$menus = "";
+		if(!isset($_GET['menus']) || $_GET['menus']==""){
+			$menus = "main";
+		}
+		else{
+			$menus = $_GET['menus'];
+		}
+		if(isset($_GET['type']) && $_GET['type']!=""){
+			$etc_ar['type'] = $_GET['type'];
+		}
+		$str = admin_controller_style($menus, $etc_ar);
 	}
 	echo admin_style($str, $type_admin);
 
