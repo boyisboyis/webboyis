67983a9f5005b9ee026f48e4243ee2bf158e2708
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-08-17 20:44:13 +0700

    add component web show in header

--- administrator/controller/addmenu.php
+++ administrator/controller/addmenu.php
@@ -9,9 +9,13 @@
 	}
 	else if($mode=='gen'){
 		gen_addmenu();
+		go_path("?menus=addmenu");
 	}
 	else if($mode=='up'){
 		menu_up();
+	}
+	else if($mode=='down'){
+		menu_down();
 	}
 	else if($mode=='show'){
 		menu_show();
@@ -52,37 +56,6 @@
 		go_path("?menus=addmenu");
 	}
 
-	function gen_addmenu(){
-		$sql = "SELECT * FROM menu, translate_menu WHERE menu.menuid=translate_menu.menuid AND menu.parentid=0 ORDER BY menu.seq";
-		$topmenu = sql_getall($sql);
-		$data = array("th" => array(),"en" => array());
-		$n = count($topmenu);
-
-		for($i=0; $i<$n; $i++){
-			$sql = "SELECT * FROM menu, translate_menu WHERE menu.menuid=translate_menu.menuid AND menu.parentid=".$topmenu[$i]['menuid']."";
-			$parentmenu = sql_getall($sql);
-			foreach ($data as $key => $value) {
-				$topdata = array(
-					"name" => $topmenu[$i]['menu_'.$key],
-					"link" => "#"
-				);
-				$n_parentmenu = count($parentmenu);
-				if($n_parentmenu > 0){
-					for($j=0;$j<$n_parentmenu;$j++){
-						$parentdata =array(
-							"name" => $parentmenu[$j]['menu_'.$key],
-							"link" => "#"
-						);
-						$topdata['parent'][] = $parentdata;
-					}
-				}
-				$data[$key][$topmenu[$i]['position']][] = $topdata;
-			}
-		}
-		gen_json_pattern($data);
-		go_path("?menus=addmenu");
-	}
-
 	function menu_up(){
 		if(!isset($_GET['menuid'])){
 			go_path($GLOBALS['adminpath']);
@@ -102,9 +75,31 @@
 		sql_simple($sql);
 		$sql = "UPDATE menu SET seq={$sequesup} WHERE parentid={$parentid} AND menuid={$menuid}";
 		sql_simple($sql);
-		echo $menuid;
-		echo $parentid;
-		echo $seques;
+		gen_addmenu();
+		go_path("?menus=addmenu");
+	}
+
+	function menu_down(){
+		if(!isset($_GET['menuid'])){
+			go_path($GLOBALS['adminpath']);
+		}
+		if(!isset($_GET['parentid'])){
+			go_path($GLOBALS['adminpath']);
+		}
+		if(!isset($_GET['seq'])){
+			go_path($GLOBALS['adminpath']);
+		}
+
+		$menuid = $_GET['menuid'];
+		$parentid = $_GET['parentid'];
+		$seques = $_GET['seq'];
+		$sequesdown = $seques + 1;
+		$sql = "UPDATE menu SET seq={$seques} WHERE parentid={$parentid} AND seq={$sequesdown}";
+		sql_simple($sql);
+		$sql = "UPDATE menu SET seq={$sequesdown} WHERE parentid={$parentid} AND menuid={$menuid}";
+		sql_simple($sql);
+		gen_addmenu();
+		go_path("?menus=addmenu");
 	}
 
 	function menu_show(){
@@ -112,9 +107,9 @@
 			go_path($GLOBALS['adminpath']);
 		}
 		$menuid = $_GET['menuid'];
-		echo $menuid;
 		$sql = "UPDATE menu SET visible = 1 WHERE menuid=".$menuid;//UPDATE `webboyis`.`menu` SET `visible` = '1' WHERE `menu`.`menuid` = 1;
 		sql_simple($sql);
+		gen_addmenu();
 		go_path("?menus=addmenu");
 	}
 
@@ -123,10 +118,48 @@
 			go_path($GLOBALS['adminpath']);
 		}
 		$menuid = $_GET['menuid'];
-		echo $menuid;
 		$sql = "UPDATE menu SET visible = 0 WHERE menuid=".$menuid;//UPDATE `webboyis`.`menu` SET `visible` = '1' WHERE `menu`.`menuid` = 1;
 		sql_simple($sql);
+		gen_addmenu();
 		go_path("?menus=addmenu");
+	}
+
+	function gen_addmenu(){
+		$sql = "SELECT * FROM menu, translate_menu WHERE menu.menuid=translate_menu.menuid AND menu.parentid=0 AND menu.visible=1 ORDER BY menu.seq";
+		$topmenu = sql_getall($sql);
+		$data = array("th" => array(),"en" => array());
+		$n = count($topmenu);
+		for($i=0; $i<$n; $i++){
+			$sql = "SELECT * FROM menu, translate_menu WHERE menu.menuid=translate_menu.menuid AND menu.visible=1 AND menu.parentid=".$topmenu[$i]['menuid']." ORDER BY menu.seq";
+			$parentmenu = sql_getall($sql);
+			foreach ($data as $key => $value) {
+				$link = $topmenu[$i]['link'];
+				if($link==""){
+					$link="/";
+				}
+				$topdata = array(
+					"name" => $topmenu[$i]['menu_'.$key],
+					"link" => $link
+				);
+				$n_parentmenu = count($parentmenu);
+				if($n_parentmenu > 0){
+					for($j=0;$j<$n_parentmenu;$j++){
+						$link = $parentmenu[$j]['link'];
+						if($link==""){
+							$link="/";
+						}
+						$parentdata =array(
+							"name" => $parentmenu[$j]['menu_'.$key],
+							"link" => $link
+						);
+						$topdata['parent'][] = $parentdata;
+					}
+				}
+				$data[$key][$topmenu[$i]['position']][] = $topdata;
+			}
+		}
+		gen_json_pattern($data);
+		//
 	}
 
 ?>
