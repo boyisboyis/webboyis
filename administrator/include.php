e1eb99b27d944b93fd56e54443873ce5192dd9d4
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-10-23 16:04:10 +0700

    repreject2

--- administrator/include.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	// require_once ('../mainfunc/html.inc');
-	// require_once ('../mainfunc/etc.inc');
-	require_once ('/func/admin_func.inc');
-	require_once ('/func/webadmin_style.inc');
-
-?>
