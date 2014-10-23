293eab2da6fda4f7883e72a437d46f9a68113428
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-10-23 19:07:19 +0700

    update etc for getpath

---
+++ include.php
@@ -0,0 +1,6 @@
+<?php
+
+	require_once 'mainphp/etc.inc';
+	require_once 'mainphp/db.inc';
+
+?>
--- travals.php
+++ travals.php
@@ -1,5 +1,11 @@
 <?php
 
-	
+	$path = Etc::getpath();
+	if(count($path)>0){
+		// travals page
+	}
+	else{
+		// main page
+	}
 
 ?>
