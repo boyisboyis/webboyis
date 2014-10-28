851fe9145b5b4a19ca6db484a246b00455064bdd
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-10-28 18:55:39 +0700

    test angular

--- administrator/adminlogin.php
+++ administrator/adminlogin.php
@@ -3,6 +3,7 @@
 	$html = new Html();
 	$basepath = Path::getbasepath();
 	//echo Path::getbasepath();
+	//about 6.30
 
 ?>
 
@@ -17,8 +18,28 @@
 		?>
 	</head>
 	<body>
-		<div id="wrapper">
-			
+		<div id="wrapper" data-ng-app=''>
+			<div id='box-login'>
+				<div>
+					<form name='loginForm' novalidate>
+						<div>
+							<span>Username: </span>
+							<input type="text" name="username" data-ng-model='username' required>
+							<span class='' data-ng-show="loginForm.username.$dirty && loginForm.username.$invalid">
+								<span data-ng-show="loginForm.username.$error.required">Required</span>
+							</span>
+						</div>
+						<div>
+							<span>Password: </span><input type="password" name='password' data-ng-model='password' required>
+						</div>
+						<div>
+							<button data-ng-disabled="loginForm.username.$invalid || loginForm.password.$invalid">Login</button>
+						</div>
+					</form>
+					<!-- <p data-ng-bind='username'></p>
+					<p data-ng-bind='password'></p> -->
+				</div> 
+			</div>
 		</div>
 	</body>
 </html>
