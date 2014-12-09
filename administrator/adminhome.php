2d3cacf117a74ac20f5a12376e6ad75941f0c6a1
Supagrid Tangsermsit <boyisboyis261129@gmail.com>
2014-12-09 19:19:06 +0700

    update

--- administrator/adminhome.php
+++ administrator/adminhome.php
@@ -1,19 +1,33 @@
 <?php
 	
-	print_r($_SESSION);
-	unset($_SESSION["member"],$_SESSION["admin"]);
 	if(!Etc::login() || !Etc::loginadmin()){
 		echo "test";
 	}
+	$html = new Html();
+	$basepath = Path::getbasepath();
 
 ?>
 
 <!DOCTYPE html>
-<html>
+<html data-ng-app="appAdminHome">
 	<head>
 		<title>Admin Page:BoyI's</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="initial-scale=1, width=device-width">
+		<link rel="stylesheet" type="text/css" href="/css/min/mainadminhome.css">
 	</head>
 	<body>
-		
+		<div id="wrapper" data-ng-controller="adminController">
+			<div id="wrap-left">
+				<div id="button-small">a</div>
+				<div id="nav"></div>
+			</div>
+			<div id="wrap-right">
+				<div id="right-header"></div>
+				<div id="right-content">aaa</div>
+			</div>
+		</div>
+		<script type="text/javascript" src="/js/ext/angular.min.js"></script>
+		<script type="text/javascript" src="/js/min/adminhome.min.js"></script>
 	</body>
 </html>
---
+++ js/adminhome.js
@@ -0,0 +1,6 @@
+//var loginApp = angular.module('loginApp', []);
+//loginApp.controller('loginController', function($scope, $http){
+var appadmin = angular.module('appAdminHome', []);
+appadmin.controller('adminController', function($scope, $http){
+	
+});
